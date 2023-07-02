0840a53d441b24660919c704dd617065294963f5
TrickEye <trickeye@buaa.edu.cn>
2023-07-02 23:47:11 +0800

    init repository

---
+++ src/components/testModule.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'testModule'
+}
+</script>
+
+<template>
+    <h1>TITLE OF TESTMODULE</h1>
+
+    <div class="card">
+        <!-- <button type="button" @click="count++">count is {{ count }}</button> -->
+        <p>
+            Edit
+            <code>components/HelloWorld.vue</code> to test HMR
+        </p>    
+    </div>
+</template>
