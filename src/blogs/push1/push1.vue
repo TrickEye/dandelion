e0ed43ebe31cbb8fc17f41d36f628fee6a1d7af7
TrickEye <trickeye@buaa.edu.cn>
2023-07-05 00:10:51 +0800

    feat: push1 partially complete

---
+++ src/blogs/push1/push1.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'push1'
+}
+</script>
+
+<template>
+<v-row>
+  <v-col cols="12" xl="6" lg="8" md="10" sm="12" xs="12" class="ma-auto">
+    <h1>扬帆起航 | 蒲公英乡野航迹实践队简介</h1>
+    <br>
+
+    <v-card class="pa-3 ma-3">
+      为深入学习贯彻<strong>习近平新时代中国特色社会主义思想</strong>，
+      全面贯彻落实<strong>党的二十大精神</strong>，
+      <strong>北航计算机学院蒲公英乡野航迹实践队</strong>将于暑假组织
+      前往<strong>山西省吕梁市中阳县</strong>，主要针对文化、教育等方面展开实地调研，
+      利用知识对当地产业赋能。
+    </v-card>
+
+    <v-card class="pa-3 ma-3">
+
+      <v-img src="./final_2.png"/></v-card>
+  </v-col>
+</v-row>
+</template>
+
+<style scoped>
+
+</style>
