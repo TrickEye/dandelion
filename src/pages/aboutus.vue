d71802f29456adb56572a7cc3693ed52be4e0ca7
TrickEye <85970705+TrickEye@users.noreply.github.com>
2023-09-17 20:29:38 +0800

    add photo

--- src/pages/aboutus.vue
+++ src/pages/aboutus.vue
@@ -6,6 +6,16 @@ import CuiYikai from '../assets/aboutus/CuiYikai.jpg'
 <script>
 import DuJinyang from '../assets/aboutus/DuJinyang.jpg'
 import CuiYikai from '../assets/aboutus/CuiYikai.jpg'
+import GaoShijun from '../assets/aboutus/GaoShijun.png'
+import HuangYu from '../assets/aboutus/HuangYu.jpg'
+import YuYang from '../assets/aboutus/YuYang.jpg'
+import KeQiyan from '../assets/aboutus/KeQiyan.jpg'
+import LiNanbin from '../assets/aboutus/LiNanbin.jpg'
+import LiYihao from '../assets/aboutus/LiYihao.png'
+import DongJiarong from '../assets/aboutus/DongJiarong.jpg'
+import HeJiayi from '../assets/aboutus/HeJiayi.jpg'
+import YinXinping from '../assets/aboutus/YinXinping.jpeg'
+
 
   export default {
     name: '蒲公英们',
@@ -17,38 +27,38 @@ import CuiYikai from '../assets/aboutus/CuiYikai.jpg'
           crew: [{
             id: 0,
             name: '高世骏',
-            pic: 'todo',
-            intro: 'todo'
+            pic: GaoShijun,
+            intro: '来自北京的高世骏，喜欢打游戏'
           }, {
             id: 1,
             name: '杜金洋',
             pic: DuJinyang,
-            intro: "This is Charles's Castle"
+            intro: "热爱生活，元气满满的阳光小子"
           }, {
             id: 2,
             name: '黄钰',
-            pic: 'todo',
+            pic: HuangYu,
             intro: 'todo'
           }, {
             id: 3,
             name: '于洋',
-            pic: 'todo',
-            intro: 'todo'
+            pic: YuYang,
+            intro: 'hello 大家好我是蒲公英乡野航迹实践队的队员于洋。'
           }, {
             id: 4,
             name: '柯棋严',
-            pic: 'todo',
+            pic: KeQiyan,
             intro: 'todo'
           }, {
             id: 5,
             name: '李南冰',
-            pic: 'todo',
-            intro: 'todo'
+            pic: LiNanbin,
+            intro: '我是李南冰，来自天津，实践活动让我感受到了新时代中阳的发展与进步。'
           }, {
             id: 6,
             name: '李一豪',
-            pic: 'todo',
-            intro: 'todo'
+            pic: LiYihao,
+            intro: '冷酷的外表下隐藏着一颗火热的心。在熟悉的人面前，我就是一个天天傻笑的半吊子。没啥特别的爱好，但是啥也愿意尝试。'
           }, {
             id: 7,
             name: '崔怿恺',
@@ -57,18 +67,18 @@ import CuiYikai from '../assets/aboutus/CuiYikai.jpg'
           }, {
             id: 8,
             name: '董佳荣',
-            pic: 'todo',
-            intro: 'todo'
+            pic: DongJiarong,
+            intro: '老二刺猿了'
           }, {
             id: 9,
             name: '何嘉怡',
-            pic: 'todo',
-            intro: 'todo'
+            pic: HeJiayi,
+            intro: '已毕业的社恐学姐， 很荣幸参加到本次实践中🌹'
           }, {
             id: 10,
             name: '尹馨平',
-            pic: 'todo',
-            intro: 'todo'
+            pic: YinXinping,
+            intro: '早安，我是一个平平无奇的乱入队友'
           }],
           show: true
         }]
@@ -92,8 +102,8 @@ import CuiYikai from '../assets/aboutus/CuiYikai.jpg'
       <div v-show="moment.show" class="pa-3">
         <v-row>
           <v-col cols="12" xl="3" lg="4" md="6" sm="12" xs="12" v-for="person in moment.crew">
-            <v-card min-height="200px" class="bg-blue-accent-1 pa-2 text-center">
-              <v-avatar size="200px">
+            <v-card class="person bg-blue-accent-1 pa-2 text-center">
+              <v-avatar size="200px" style="margin-top: 15px">
                 <v-img v-if="person.pic !== 'todo'" :cover="true" :src="person.pic" size="200px"></v-img>
                 <v-icon v-else size="200px">mdi-account</v-icon>
               </v-avatar>
@@ -110,5 +120,7 @@ import CuiYikai from '../assets/aboutus/CuiYikai.jpg'
 </template>
 
 <style scoped>
-
+.person {
+  height: 350px;
+}
 </style>
